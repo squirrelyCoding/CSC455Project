8374ec017cde17f20306cba546226a02780e010a
squirrelyCoding <georgeabbie0@gmail.com>
2025-10-30 15:56:45 -0500

    Ag: trying something real quick. Shouldn't mess things up though

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+arp_table = pd.DataFrame({
+    'IP_Address': ['192.168.1.1', '192.168.1.10', '192.168.1.11', '192.168.1.1'],
+    'MAC_Address': ['AA:BB:CC:11:22:33', 'EE:FF:GG:44:55:66', 'HH:II:JJ:77:88:99', 'ZZ:YY:XX:11:22:33']
+})
+
+print("Simulated ARP Table:\n", arp_table, "\n")
+
+suspicious = arp_table.groupby('IP_Address').filter(lambda x: x['MAC_Address'].nunique() > 1)
+
+if not suspicious.empty:
+    print("Warning: Possible ARP spoofing detected!")
+    print(suspicious)
+else:
+    print("No duplicate IP-MAC mappings found.")
