bcf2802ac90e25c1e38d610260aeb8f2dd21afa2
Carmindy-P <cphillips30@murraystate.edu>
2025-10-31 00:15:51 +0000

    changed to streamlit

--- src/gui.py
+++ src/gui.py
@@ -1,1 +1,9 @@
-import tkinter as tk
+import streamlit as st
+
+
+def run_app():
+    st.title("My First Web App")
+    name = st.text_input("Enter your name:")
+    if st.button("Greet"):
+        st.write(f"Hello, {name}!")
+    
