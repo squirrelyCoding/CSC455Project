094bf6fafb0c99e5d54b2b8b67a742032eee732a
idaugherty <isabelladaugherty5@gmail.com>
2025-11-05 10:12:43 -0500

    changed logic for pull request

---
+++ src/hash.py
@@ -0,0 +1,17 @@
+import bcrypt
+
+def  HashPassword():
+    #input a password
+    #the password is transformed into a byte object because bcrypt expects bytes
+    password = input().encode('utf-8')
+
+    #generate a salt
+    #rounds parameter controls computational cost (higher == slower but more secure)
+    salt = bcrypt.gensalt(rounds=12)
+    #hashes password using hashpw method
+    #result of this is a byte object
+    hashedPassword = bcrypt.hashpw(password, salt)
+
+    print(hashedPassword)
+
+HashPassword()
--- src/main.py
+++ src/main.py
@@ -1,17 +1,3 @@
-import bcrypt
+import hash
 
-def  HashPassword():
-    #input a password
-    #the password is transformed into a byte object because bcrypt expects bytes
-    password = input().encode('utf-8')
-
-    #generate a salt
-    #rounds parameter controls computational cost (higher == slower but more secure)
-    salt = bcrypt.gensalt(rounds=12)
-    #hashes password using hashpw method
-    #result of this is a byte object
-    hashedPassword = bcrypt.hashpw(password, salt)
-
-    print(hashedPassword)
-
-HashPassword()
+hash.HashPassword()
